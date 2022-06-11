14fa20694ace2042cb163a48b20f4f446d0b5df3
Tony Paterson <tony@cantabrian.co.nz>
2022-06-12 08:16:23 +1200

    misc

---
+++ ADO Process Tools/export_ado_process.py
@@ -0,0 +1,10 @@
+#
+# Export an Azure DevOps process
+# ==============================
+#
+
+import logging
+import zoyinc_std_tools
+ 
+logger = zoyinc_std_tools.enableLogging('debug','warning','c:/temp/ado_process_logging.log')
+logger.warning('Hello')
--- ADO Process Tools/zoyinc_std_tools.py
+++ ADO Process Tools/zoyinc_std_tools.py
@@ -1,3 +1,48 @@
 #
 # Standard tools
 # ==============
+#
+
+import logging
+
+def enableLogging(consoleLogLevelRaw, fileLogLevelRaw, logFilename):
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # Misc
+    consoleLogLevel = consoleLogLevelRaw.upper().strip()
+    fileLogLevel = fileLogLevelRaw.upper().strip()
+    validLogLevels = ['DEBUG','INFO','WARNING','ERROR','CRITICAL']
+
+    # Enable logging handlers
+    consoleLogHandler = logging.StreamHandler()
+    fileLogHandler = logging.FileHandler(logFilename, mode='a', encoding=None, delay=False, errors=None)
+
+    # Log formatters
+    consoleLogHandler.setFormatter(logging.Formatter('%(message)s'))
+    fileLogHandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s', '%Y/%m/%d %H:%M:%S'))
+    
+    # Set logging levels
+    if consoleLogLevel in validLogLevels:
+        print('# Setting console log level of ' + consoleLogLevel)
+        consoleLogHandler.setLevel(logging.DEBUG)
+    else:
+        print('#', flush=True)
+        print('# Console log level \'' + consoleLogLevel + '\' is invalid!', flush=True)
+        print('#', flush=True)
+        exit(1)
+    if fileLogLevel in validLogLevels:
+        print('# Setting file log level of ' + consoleLogLevel)
+        fileLogHandler.setLevel(logging.DEBUG)
+    else:
+        print('#', flush=True)
+        print('# File log level \'' + fileLogLevel + '\' is invalid!', flush=True)
+        print('#', flush=True)
+        exit(1)
+
+    # Add handlers to logger
+    logger.addHandler(consoleLogHandler)
+    logger.addHandler(fileLogHandler)
+
+    return logger
