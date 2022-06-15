c8d9c95f4da31b3ed59b840297378fc69251cbbe
Tony Paterson <tony@cantabrian.co.nz>
2022-06-15 22:11:00 +1200

    misc

--- ADO Process Tools/export_ado_process.py
+++ ADO Process Tools/export_ado_process.py
@@ -3,8 +3,69 @@
 # ==============================
 #
 
+import argparse
 import logging
+import sys
+import json
+from re import T
 import zoyinc_std_tools
- 
-logger = zoyinc_std_tools.enableLogging('debug','warning','c:/temp/ado_process_logging.log')
-logger.warning('Hello')
+
+#
+# User defined variables
+#
+consoleLogLevel = 'info'
+fileLogLevel = 'debug'
+logFilename = 'c:/temp/ado_process_logging.log'
+proxySvr = 'http://192.168.202.245:80'
+adoOrg = 'zoyinc'
+adoProjectRaw = 'examples'
+scriptOk = True
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-azuretoken", required=True, help="Azure personal access token PAL")
+
+args = parser.parse_args()
+azureToken = args.azuretoken
+processDict = {}
+
+
+#
+# Misc variables
+#
+proxySvr = {'http':proxySvr, 'https':proxySvr}
+logger = zoyinc_std_tools.enableLogging(consoleLogLevel,fileLogLevel,logFilename)
+adoProject = adoProjectRaw.lower().strip()
+
+# # Get list of projects in org
+# adoApiUrl = 'https://dev.azure.com/' + adoOrg + '/_apis/projects?api-version=6.0'
+# projectDetails = zoyinc_std_tools.adoAPICall(logger, adoApiUrl, 'get', None, None, None, proxySvr, azureToken, True)
+# projectID = -1
+# for curProject in projectDetails['json']['value']:
+#     if adoProject == curProject['name'].lower().strip():
+#             projectID = curProject['id']
+#             logger.debug('Project ID for \'' + adoProjectRaw + '\' = \'' + projectID + '\'.')
+# if projectID == -1:
+#     errorMsg = 'Given project name, \'' + adoProjectRaw + '\', does not exist in the \'' + adoOrg + '\' organization.'
+#     scriptOk = False
+
+# Get list of processes in org
+adoApiUrl = 'https://dev.azure.com/' + adoOrg + '/_apis/work/processes?api-version=6.0-preview.2'
+processDetails = zoyinc_std_tools.adoAPICall(logger, adoApiUrl, 'get', None, None, None, proxySvr, azureToken, True)
+processDict.update({'process':{}})
+for curProcess in processDetails['json']['value']:
+    processDict['process'].update({curProcess['name'].lower():curProcess})
+print( json.dumps(processDict, indent=4, sort_keys=True))
+
+
+if scriptOk:
+    logger.info('Script completed successfully.')
+else:
+    logger.error('#')
+    logger.error('# Script failed with the following error')
+    logger.error('#')
+    for currLine in errorMsg.splitlines():
+        logger.error(currLine)
+    logger.error('')
+    sys.stdout.flush()
+    exit(1)
--- ADO Process Tools/zoyinc_std_tools.py
+++ ADO Process Tools/zoyinc_std_tools.py
@@ -4,7 +4,96 @@
 #
 
 import logging
+import requests
+import sys
+import json
 
+#
+# Azure DevOps REST api call
+#
+# Requests docs: https://requests.readthedocs.io/en/latest/api/
+#
+def adoAPICall(logger, requestURL, requestTypeRaw, requestParams, requestData, requestHeaders, requestProxies, azureToken, failOnError):
+
+    currFunction = __name__ + '.adoAPICall()'
+    logger.debug('running function ' + currFunction)
+    errorsFound = False
+
+    requestType = requestTypeRaw.upper()
+    if requestType not in ['GET', ]:
+        logger.error('#')
+        logger.error('# Request type \'' + requestType + '\' not currently supported in this function \'' + currFunction + '\'.')
+        logger.error('#')
+        sys.stdout.flush()
+        exit(1)
+
+    if requestType == 'GET':
+        logger.debug('Making request.' + requestType.lower() + '() call')
+        logger.debug('URL: ' + requestURL)
+        try:
+            adoResponse = requests.get(requestURL, params=requestParams, data=requestData, headers=requestHeaders, proxies = requestProxies, auth=('', azureToken), )
+            logger.debug('Response.content: ' + str(adoResponse.content))
+        except requests.exceptions.ProxyError as e:
+            errorsFound = True
+            errorMsg = 'Failed to load json response.\nError:' + str(e)       
+
+    # Check the response
+    if not errorsFound: 
+        if adoResponse.status_code == 200:        
+            try:
+                responseJson = json.loads(adoResponse.content)
+            except ValueError as e:
+                errorsFound = True
+                errorMsg = 'Failed to load json response.\nError:' + str(e)
+                logger.error('# Error making api call: ' + errorMsg)
+        else:
+            if failOnError:
+                errorsFound = True                
+                errorMsg = 'api returned response code: ' + str(adoResponse.status_code) + '\nThe response was:\n' + str(adoResponse.content)
+                logger.error('# Error making api call: ' + errorMsg)
+        
+    if errorsFound:
+        if failOnError:
+            # If fail on an error
+            responseJson = {'errorMsg':errorMsg, 'success':False}
+            logger.error('#')
+            logger.error('# Error running ' + currFunction)
+            logger.error('#')
+            for currLine in errorMsg.splitlines():
+                logger.error(currLine)
+            logger.error('')
+            sys.stdout.flush()
+            exit(1)
+        else:
+            # If do not fail on error
+            logger.error('#')
+            logger.error('# Error running ' + currFunction)
+            logger.error('# failOnError is set to False, so we won\'t fail, however the error is:')
+            logger.error('#')
+            for currLine in errorMsg.splitlines():
+                logger.error(currLine)
+            logger.error()
+            sys.stdout.flush()
+    
+    if errorsFound:
+        adoApiReturn = {'json':None,
+                    'status_code':adoResponse.status_code,
+                    'content':adoResponse.content,
+                    'errorMsg':errorMsg,
+                    'success':False}
+    else:
+        adoApiReturn = {'json':responseJson,
+                    'status_code':adoResponse.status_code,
+                    'content':adoResponse.content,
+                    'errorMsg':None,
+                    'success':True}
+    
+    return adoApiReturn
+
+
+#
+# Enable logging
+#
 def enableLogging(consoleLogLevelRaw, fileLogLevelRaw, logFilename):
 
     logger = logging.getLogger(__name__)
@@ -25,16 +114,14 @@ def enableLogging(consoleLogLevelRaw, fileLogLevelRaw, logFilename):
     
     # Set logging levels
     if consoleLogLevel in validLogLevels:
-        print('# Setting console log level of ' + consoleLogLevel)
-        consoleLogHandler.setLevel(logging.DEBUG)
+        consoleLogHandler.setLevel(consoleLogLevel)
     else:
         print('#', flush=True)
         print('# Console log level \'' + consoleLogLevel + '\' is invalid!', flush=True)
         print('#', flush=True)
         exit(1)
     if fileLogLevel in validLogLevels:
-        print('# Setting file log level of ' + consoleLogLevel)
-        fileLogHandler.setLevel(logging.DEBUG)
+        fileLogHandler.setLevel(fileLogLevel)
     else:
         print('#', flush=True)
         print('# File log level \'' + fileLogLevel + '\' is invalid!', flush=True)
